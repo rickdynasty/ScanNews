abd2cb260a92ff9981774e6c640f0a11460d53c6
bazhancong <1024981979@qq.com>
2017-09-15 23:33:18 +0800

    增加设置选项

---
+++ app/src/main/java/com/example/eight/scannews/view/SettingsActivity.java
@@ -0,0 +1,35 @@
+package com.example.eight.scannews.view;
+
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.example.eight.scannews.R;
+
+public class SettingsActivity extends AppCompatActivity {
+    ActionBar actionBar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+        actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                finish();
+                break;
+
+            default:
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
